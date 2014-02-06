9f952a2a9839c208ae0db7aba8280b1291f2f8da
codemania113 <paridhi2011@gmail.com>
2014-02-06 18:43:08 +0530

    after review 5

--- tree/tree/project_views.py
+++ tree/tree/project_views.py
@@ -2,37 +2,36 @@ import json
 
 import django
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
 def load(request, addr):
     with open('json1.json', 'r') as json1_file:
-        json1_data = json.load(json1_file)
-        node = json1_data
-        check = 0
-        a = 0
-        lis = request.path.strip('/').split("/")
-        for tag in lis:
-            if check == 0:
-                check = 1
+        node = json.load(json1_file)
+        check_root = 0
+        lis = addr.strip('/').split("/")
+        for element in lis:
+            # condition to enter in root node, which is in dictionary format
+            if check_root == 0:
+                check_root = 1
+                prev_element = element
                 if node['slug'] == lis[-1]:
                     break
+            # condition to enter in rest of the cases which are in list format
             else:
-                    if node['slug'] == lis[a] and node['kind'] == 'Topic':
-                        b = a + 1
-                        for child in node['children']:
-                            if child['slug'] == lis[b]:
-                                node = child
-                                a = a + 1
-                                break
-                            else:
-                                continue
+                # condition to check that we are entering right url, else it will redirect to page not found
+                if node['slug'] == prev_element and node['kind'] == 'Topic':
+                    for child in node['children']:
+                        if child['slug'] == element:
+                            node = child
+                            break
+                prev_element = element
+        # the last slug in the list is checked here
         if node['slug'] == lis[-1]:
-            link = settings.MEDIA_URL + "styles.css"
             if node['kind'] == 'Topic':
-                return render(request, 'parent.html', {'node': node, 'link': link})
+                return render(request, 'parent.html', {'node': node})
             else:
-                return render(request, 'child.html', {'node': node, 'link': link})
+                return render(request, 'child.html', {'node': node})
         else:
-            return HttpResponse("Invalid Url")
+            return HttpResponseNotFound('<h1>No Page Here</h1>')
