aaa3e6425ead29ded25526ccd140e063260fbea0
codemania113 <paridhi2011@gmail.com>
2014-02-07 20:40:13 +0530

    Update project_views.py

--- tree/tree/project_views.py
+++ tree/tree/project_views.py
@@ -9,24 +9,19 @@ from django.shortcuts import render
 def load(request, addr):
     with open('json1.json', 'r') as json1_file:
         node = json.load(json1_file)
-        check_root = 0
         lis = addr.strip('/').split("/")
-        for element in lis:
-            # condition to enter in root node, which is in dictionary format
-            if check_root == 0:
-                check_root = 1
-                prev_element = element
-                if node['slug'] == lis[-1]:
-                    break
-            # condition to enter in rest of the cases which are in list format
+        if node['slug'] == lis[0]:
+            if node['slug'] == lis[-1]:
+                return render(request, 'parent.html', {'node': node})
             else:
-                # condition to check that we are entering right url, else it will redirect to page not found
-                if node['slug'] == prev_element and node['kind'] == 'Topic':
-                    for child in node['children']:
-                        if child['slug'] == element:
-                            node = child
-                            break
-                prev_element = element
+                prev_element = lis[0]
+                for element in lis[1:]:
+                    if node['slug'] == prev_element and node['kind'] == 'Topic':
+                        for child in node['children']:
+                            if child['slug'] == element:
+                                node = child
+                                break
+                    prev_element = element
         # the last slug in the list is checked here
         if node['slug'] == lis[-1]:
             if node['kind'] == 'Topic':
